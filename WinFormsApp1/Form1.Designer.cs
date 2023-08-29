2ce0d5fdc7226d33590efc3b4b538d963731ab98
leoquinino <leonardo.quinino@gmail.com>
2023-08-29 16:33:15 -0300

    Adicionar arquivos de projeto.

---
+++ WinFormsApp1/Form1.Designer.cs
@@ -0,0 +1,395 @@
+﻿namespace WinFormsApp1
+{
+    partial class FrmMeuAppTestes
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.PageBotoes = new System.Windows.Forms.TabPage();
+            this.textBox = new System.Windows.Forms.TextBox();
+            this.btn_Sair = new System.Windows.Forms.Button();
+            this.btn_ok = new System.Windows.Forms.Button();
+            this.PageCalcular = new System.Windows.Forms.TabPage();
+            this.bt_ok_parcelas = new System.Windows.Forms.Button();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.lblResultado = new System.Windows.Forms.Label();
+            this.lblParcela2 = new System.Windows.Forms.Label();
+            this.lblParcela1 = new System.Windows.Forms.Label();
+            this.PageNotasMedia = new System.Windows.Forms.TabPage();
+            this.label6 = new System.Windows.Forms.Label();
+            this.numericUpDownMedia = new System.Windows.Forms.NumericUpDown();
+            this.btCalcMedia = new System.Windows.Forms.Button();
+            this.textBoxMedia = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBoxNota4 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBoxNota3 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBoxNota2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBoxNota1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lbResultado = new System.Windows.Forms.Label();
+            this.tabControl1.SuspendLayout();
+            this.PageBotoes.SuspendLayout();
+            this.PageCalcular.SuspendLayout();
+            this.PageNotasMedia.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownMedia)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tabControl1.Controls.Add(this.PageBotoes);
+            this.tabControl1.Controls.Add(this.PageCalcular);
+            this.tabControl1.Controls.Add(this.PageNotasMedia);
+            this.tabControl1.Location = new System.Drawing.Point(3, 3);
+            this.tabControl1.Multiline = true;
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(632, 293);
+            this.tabControl1.TabIndex = 3;
+            // 
+            // PageBotoes
+            // 
+            this.PageBotoes.Controls.Add(this.textBox);
+            this.PageBotoes.Controls.Add(this.btn_Sair);
+            this.PageBotoes.Controls.Add(this.btn_ok);
+            this.PageBotoes.Location = new System.Drawing.Point(4, 24);
+            this.PageBotoes.Name = "PageBotoes";
+            this.PageBotoes.Padding = new System.Windows.Forms.Padding(3);
+            this.PageBotoes.Size = new System.Drawing.Size(624, 265);
+            this.PageBotoes.TabIndex = 0;
+            this.PageBotoes.Text = "Botões";
+            this.PageBotoes.UseVisualStyleBackColor = true;
+            // 
+            // textBox
+            // 
+            this.textBox.Location = new System.Drawing.Point(29, 16);
+            this.textBox.Name = "textBox";
+            this.textBox.Size = new System.Drawing.Size(166, 23);
+            this.textBox.TabIndex = 5;
+            // 
+            // btn_Sair
+            // 
+            this.btn_Sair.Location = new System.Drawing.Point(120, 57);
+            this.btn_Sair.Name = "btn_Sair";
+            this.btn_Sair.Size = new System.Drawing.Size(75, 23);
+            this.btn_Sair.TabIndex = 4;
+            this.btn_Sair.Text = "Sair";
+            this.btn_Sair.UseVisualStyleBackColor = true;
+            this.btn_Sair.Click += new System.EventHandler(this.btn_Sair_Click);
+            // 
+            // btn_ok
+            // 
+            this.btn_ok.Location = new System.Drawing.Point(29, 57);
+            this.btn_ok.Name = "btn_ok";
+            this.btn_ok.Size = new System.Drawing.Size(75, 23);
+            this.btn_ok.TabIndex = 3;
+            this.btn_ok.Text = "Ok";
+            this.btn_ok.UseVisualStyleBackColor = true;
+            this.btn_ok.Click += new System.EventHandler(this.btn_ok_Click);
+            // 
+            // PageCalcular
+            // 
+            this.PageCalcular.Controls.Add(this.bt_ok_parcelas);
+            this.PageCalcular.Controls.Add(this.textBox3);
+            this.PageCalcular.Controls.Add(this.textBox2);
+            this.PageCalcular.Controls.Add(this.textBox1);
+            this.PageCalcular.Controls.Add(this.lblResultado);
+            this.PageCalcular.Controls.Add(this.lblParcela2);
+            this.PageCalcular.Controls.Add(this.lblParcela1);
+            this.PageCalcular.Location = new System.Drawing.Point(4, 24);
+            this.PageCalcular.Name = "PageCalcular";
+            this.PageCalcular.Padding = new System.Windows.Forms.Padding(3);
+            this.PageCalcular.Size = new System.Drawing.Size(624, 265);
+            this.PageCalcular.TabIndex = 1;
+            this.PageCalcular.Text = "Parcelas";
+            this.PageCalcular.UseVisualStyleBackColor = true;
+            // 
+            // bt_ok_parcelas
+            // 
+            this.bt_ok_parcelas.Location = new System.Drawing.Point(197, 91);
+            this.bt_ok_parcelas.Name = "bt_ok_parcelas";
+            this.bt_ok_parcelas.Size = new System.Drawing.Size(79, 23);
+            this.bt_ok_parcelas.TabIndex = 6;
+            this.bt_ok_parcelas.Text = "Ok";
+            this.bt_ok_parcelas.UseVisualStyleBackColor = true;
+            this.bt_ok_parcelas.Click += new System.EventHandler(this.bt_ok_parcelas_Click);
+            // 
+            // textBox3
+            // 
+            this.textBox3.BackColor = System.Drawing.SystemColors.Window;
+            this.textBox3.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.textBox3.Location = new System.Drawing.Point(81, 91);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 23);
+            this.textBox3.TabIndex = 5;
+            // 
+            // textBox2
+            // 
+            this.textBox2.BackColor = System.Drawing.SystemColors.Info;
+            this.textBox2.Location = new System.Drawing.Point(81, 41);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 23);
+            this.textBox2.TabIndex = 4;
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.textBox1.Location = new System.Drawing.Point(81, 8);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 23);
+            this.textBox1.TabIndex = 3;
+            // 
+            // lblResultado
+            // 
+            this.lblResultado.AutoSize = true;
+            this.lblResultado.Location = new System.Drawing.Point(21, 99);
+            this.lblResultado.Name = "lblResultado";
+            this.lblResultado.Size = new System.Drawing.Size(59, 15);
+            this.lblResultado.TabIndex = 2;
+            this.lblResultado.Text = "Resultado";
+            // 
+            // lblParcela2
+            // 
+            this.lblParcela2.AutoSize = true;
+            this.lblParcela2.Location = new System.Drawing.Point(21, 49);
+            this.lblParcela2.Name = "lblParcela2";
+            this.lblParcela2.Size = new System.Drawing.Size(54, 15);
+            this.lblParcela2.TabIndex = 1;
+            this.lblParcela2.Text = "Parcela 2";
+            // 
+            // lblParcela1
+            // 
+            this.lblParcela1.AutoSize = true;
+            this.lblParcela1.Location = new System.Drawing.Point(21, 11);
+            this.lblParcela1.Name = "lblParcela1";
+            this.lblParcela1.Size = new System.Drawing.Size(54, 15);
+            this.lblParcela1.TabIndex = 0;
+            this.lblParcela1.Text = "Parcela 1";
+            // 
+            // PageNotasMedia
+            // 
+            this.PageNotasMedia.Controls.Add(this.lbResultado);
+            this.PageNotasMedia.Controls.Add(this.label6);
+            this.PageNotasMedia.Controls.Add(this.numericUpDownMedia);
+            this.PageNotasMedia.Controls.Add(this.btCalcMedia);
+            this.PageNotasMedia.Controls.Add(this.textBoxMedia);
+            this.PageNotasMedia.Controls.Add(this.label5);
+            this.PageNotasMedia.Controls.Add(this.textBoxNota4);
+            this.PageNotasMedia.Controls.Add(this.label4);
+            this.PageNotasMedia.Controls.Add(this.textBoxNota3);
+            this.PageNotasMedia.Controls.Add(this.label3);
+            this.PageNotasMedia.Controls.Add(this.textBoxNota2);
+            this.PageNotasMedia.Controls.Add(this.label2);
+            this.PageNotasMedia.Controls.Add(this.textBoxNota1);
+            this.PageNotasMedia.Controls.Add(this.label1);
+            this.PageNotasMedia.Location = new System.Drawing.Point(4, 24);
+            this.PageNotasMedia.Name = "PageNotasMedia";
+            this.PageNotasMedia.Padding = new System.Windows.Forms.Padding(3);
+            this.PageNotasMedia.Size = new System.Drawing.Size(624, 265);
+            this.PageNotasMedia.TabIndex = 2;
+            this.PageNotasMedia.Text = "Médias";
+            this.PageNotasMedia.UseVisualStyleBackColor = true;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(323, 15);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(72, 30);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "Média  para\r\nAprovação ?";
+            // 
+            // numericUpDownMedia
+            // 
+            this.numericUpDownMedia.Location = new System.Drawing.Point(401, 22);
+            this.numericUpDownMedia.Name = "numericUpDownMedia";
+            this.numericUpDownMedia.Size = new System.Drawing.Size(61, 23);
+            this.numericUpDownMedia.TabIndex = 11;
+            this.numericUpDownMedia.Value = new decimal(new int[] {
+            7,
+            0,
+            0,
+            0});
+            // 
+            // btCalcMedia
+            // 
+            this.btCalcMedia.Location = new System.Drawing.Point(175, 157);
+            this.btCalcMedia.Name = "btCalcMedia";
+            this.btCalcMedia.Size = new System.Drawing.Size(75, 23);
+            this.btCalcMedia.TabIndex = 10;
+            this.btCalcMedia.Text = "Calcular";
+            this.btCalcMedia.UseVisualStyleBackColor = true;
+            this.btCalcMedia.Click += new System.EventHandler(this.btCalcMedia_Click);
+            // 
+            // textBoxMedia
+            // 
+            this.textBoxMedia.Location = new System.Drawing.Point(57, 156);
+            this.textBoxMedia.Name = "textBoxMedia";
+            this.textBoxMedia.Size = new System.Drawing.Size(100, 23);
+            this.textBoxMedia.TabIndex = 9;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(11, 161);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(40, 15);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Média";
+            // 
+            // textBoxNota4
+            // 
+            this.textBoxNota4.Location = new System.Drawing.Point(57, 97);
+            this.textBoxNota4.Name = "textBoxNota4";
+            this.textBoxNota4.Size = new System.Drawing.Size(100, 23);
+            this.textBoxNota4.TabIndex = 7;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(11, 102);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(39, 15);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Nota4";
+            // 
+            // textBoxNota3
+            // 
+            this.textBoxNota3.Location = new System.Drawing.Point(57, 68);
+            this.textBoxNota3.Name = "textBoxNota3";
+            this.textBoxNota3.Size = new System.Drawing.Size(100, 23);
+            this.textBoxNota3.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(11, 73);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(39, 15);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Nota3";
+            // 
+            // textBoxNota2
+            // 
+            this.textBoxNota2.Location = new System.Drawing.Point(57, 39);
+            this.textBoxNota2.Name = "textBoxNota2";
+            this.textBoxNota2.Size = new System.Drawing.Size(100, 23);
+            this.textBoxNota2.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(11, 44);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(39, 15);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Nota2";
+            // 
+            // textBoxNota1
+            // 
+            this.textBoxNota1.Location = new System.Drawing.Point(57, 10);
+            this.textBoxNota1.Name = "textBoxNota1";
+            this.textBoxNota1.Size = new System.Drawing.Size(100, 23);
+            this.textBoxNota1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(11, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(39, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Nota1";
+            // 
+            // lbResultado
+            // 
+            this.lbResultado.AutoSize = true;
+            this.lbResultado.Location = new System.Drawing.Point(13, 208);
+            this.lbResultado.Name = "lbResultado";
+            this.lbResultado.Size = new System.Drawing.Size(59, 15);
+            this.lbResultado.TabIndex = 13;
+            this.lbResultado.Text = "Resultado";
+            // 
+            // FrmMeuAppTestes
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(634, 292);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "FrmMeuAppTestes";
+            this.Text = "Aplicação Testes Eventos Button";
+            this.tabControl1.ResumeLayout(false);
+            this.PageBotoes.ResumeLayout(false);
+            this.PageBotoes.PerformLayout();
+            this.PageCalcular.ResumeLayout(false);
+            this.PageCalcular.PerformLayout();
+            this.PageNotasMedia.ResumeLayout(false);
+            this.PageNotasMedia.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownMedia)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage PageBotoes;
+        private System.Windows.Forms.TextBox textBox;
+        private System.Windows.Forms.Button btn_Sair;
+        private System.Windows.Forms.Button btn_ok;
+        private System.Windows.Forms.TabPage PageCalcular;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label lblResultado;
+        private System.Windows.Forms.Label lblParcela2;
+        private System.Windows.Forms.Label lblParcela1;
+        private System.Windows.Forms.Button bt_ok_parcelas;
+        private System.Windows.Forms.TabPage PageNotasMedia;
+        private System.Windows.Forms.TextBox textBoxNota2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBoxNota1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btCalcMedia;
+        private System.Windows.Forms.TextBox textBoxMedia;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBoxNota4;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBoxNota3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.NumericUpDown numericUpDownMedia;
+        private System.Windows.Forms.Label lbResultado;
+    }
+}
---
+++ WinFormsApp1/Form1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinFormsApp1
+{
+    public partial class FrmMeuAppTestes : Form
+    {
+        //modificadores
+        string MeuNome = "LeoQ";
+        int valor1;
+        int valor2;
+        int res;
+
+        decimal nota1, nota2, nota3, nota4, media;
+
+        private void btCalcMedia_Click(object sender, EventArgs e)
+        {
+            nota1 = decimal.Parse(textBoxNota1.Text);
+            nota2 = decimal.Parse(textBoxNota2.Text);
+            nota3 = decimal.Parse(textBoxNota3.Text);
+            nota4 = decimal.Parse(textBoxNota4.Text);
+            media = (nota1 + nota2 + nota3 + nota4) / 4;
+            textBoxMedia.Text = Convert.ToString(media);
+
+            if (media >= numericUpDownMedia.Value)            
+                lbResultado.Text = "Parabéns Aprovado !";
+            else
+                lbResultado.Text = "Infelizmente Reprovado !";
+        }
+
+        public FrmMeuAppTestes()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_ok_Click(object sender, EventArgs e)
+        {
+            textBox.Text = MeuNome;
+        }
+
+        private void btn_Sair_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void bt_ok_parcelas_Click(object sender, EventArgs e)
+        {
+            valor1 = int.Parse(textBox1.Text);
+            valor2 = int.Parse(textBox2.Text);
+            res = valor1 + valor2;
+            textBox3.Text = Convert.ToString(res);
+        }
+    }
+}
